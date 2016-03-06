2e8ad3c547aad28bc950890edeb72ceaabfe22c7
Mr.Huang <uestc_ccse@outlook.com>
2016-03-06 21:35:55 +0800

    add a colorful progress

--- script.js
+++ script.js
@@ -10,17 +10,14 @@ window.onload = function(){
 	number = document.getElementById("number");
 	cube = document.getElementById("cube");
 	outsideWidth = getCssStyle(cube,"width");
-	//
-	leftDiv = document.getElementById("leftDiv");
-	rightDiv = document.getElementById("rightDiv");
-	currentLeftRotate = getCssStyle(leftDiv,"transform");
+	// leftDiv = document.getElementById("leftDiv");
+	// rightDiv = document.getElementById("rightDiv");
+	// currentLeftRotate = getCssStyle(leftDiv,"transform");
 	setInterval(function(){
-		console.log(getCssStyle(rightDiv,"transform"));
-		//console.log(getCssStyle(leftDiv,"transform"));
+		//console.log(getCssStyle(rightDiv,"transform"));
+		// console.log(getCssStyle(leftDiv,"transform"));
 		innersideWidth = getCssStyle(progressbar,"width");
 		percentage = parseFloat(innersideWidth) / parseFloat(outsideWidth) * 100;
 		progressbar.innerHTML = Math.round(percentage) + "%";
 	},100);
-
 }
-	
